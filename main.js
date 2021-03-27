89bcc6adb73422e55c4ec3ee42b0b1c10fcfb566
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-03-27 12:43:58 +0100

    palindrome function

--- main.js
+++ main.js
@@ -6,5 +6,12 @@ const div = (a, b) => a/b;
 
 const reverseString = (...str) => str.reverse().join("");
 
+const isPalindrome = str => {
+    let reversed = str.split("").reverse().join("");
+    return (str == reversed) ? "true" : "false";
+}
+
+console.log(isPalindrome("palindrome"));
+
 
 module.exports = {add, sub, div, reverseString};
