4927e0789839f37f48c1bf0bb4ab4768580f6030
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-03-27 12:40:41 +0100

    reversed string and test

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const add = (a, b) => a+b; 
 
-const sub = (a, b) => a-b 
+const sub = (a, b) => a-b; 
 
 const div = (a, b) => a/b; 
 
-let reverseString = (...str) => str.reverse().join("");
-
+const reverseString = (...str) => str.reverse().join("");
 
 
 module.exports = {add, sub, div, reverseString};
-console.log(module.exports)
--- main.test.js
+++ main.test.js
@@ -1,11 +1,16 @@
+const { test, expect } = require("@jest/globals");
 const functions = require("./main");
 const add = functions.add;
 const sub = functions.sub;
 const div = functions.div;
+const reverseString = functions.reverseString;
 
-
-test("testint maths funcions", () => {
+test("testing maths functions", () => {
     expect(add(6,4)).toBe(10)
     expect(sub(10,4)).toBe(6)
     expect(div(6,3)).toBe(2)
+})
+
+test("to see if function returns string reversed", () => {
+    expect(reverseString(..."string to reverse")).toBe("esrever ot gnirts")
 })
