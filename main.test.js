d1b8fe2bd22b4afc03c96c30841a1f92f6ed7f1f
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-03 20:33:59 +0200

    hand-in version

--- main.test.js
+++ main.test.js
@@ -5,14 +5,8 @@ const sub = functions.sub;
 const div = functions.div;
 
 
-test("adding a with b", () => {
-    expect(add(6,4)).toBe(10);
-});
+test("adding a with b", () => expect(add(6,4)).toBe(10));
 
-test("subtracting a with b", () => {
-    expect(sub(10,4)).toBe(6);
-});
+test("subtracting a with b", () => expect(sub(10,4)).toBe(6));
 
-test("dividing a with b", () => {
-    expect(div(6,3)).toBe(2);
-});
+test("dividing a with b", () => expect(div(6,3)).toBe(2));
--- vg.test.js
+++ vg.test.js
@@ -5,11 +5,11 @@ const reverseString = VGfunctions.reverseString;
 const isPalindrome = VGfunctions.isPalindrome;
 
 test("return string reversed", () => {
-    expect(reverseString(..."string to reverse")).toBe("esrever ot gnirts")
+    expect(reverseString(..."string to reverse")).toBe("esrever ot gnirts");
 });
 
 test("return true if is palindrome and false if not", () => {
-   expect(isPalindrome("rotator")).toBe(true)
+   expect(isPalindrome("rotator")).toBe(true);
    expect(isPalindrome("notPalindrome")).toBe(false);
 });
 
